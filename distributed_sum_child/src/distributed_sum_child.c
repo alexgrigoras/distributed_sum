e7f50352e3ce774d6a66b05283f4d840d37d3d4e
Alexandru Grigoras <AG-MobileStation>
2020-12-14 19:56:49 +0200

    Updated send_array to have m and process id and corrected errors

--- distributed_sum_child/src/distributed_sum_child.c
+++ distributed_sum_child/src/distributed_sum_child.c
@@ -10,6 +10,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 #include "mpi.h"
 
 void print_array(int *array, int n, int myrank)
@@ -38,15 +39,15 @@ int compute_sum(int *array, int n)
 int main( int argc, char *argv[] ) {
 	int myrank;
 	int *array, *sum_array, *sub_array;
+	int m;
 	int n;
 	int k;
 	int sum = 0;
-	int tag = 0;
-	int nm_array[2];
+	int send_array[4];
 	int NR_WORKERS = 2;
+	int root = 0;
 	MPI_Comm parentcomm;
-	MPI_Comm workercomm2;
-	MPI_Status status ;	/* return status for receive */
+	MPI_Comm workercomm;
 
 	MPI_Init( &argc, &argv );
 	MPI_Comm_rank( MPI_COMM_WORLD, &myrank );
@@ -54,63 +55,70 @@ int main( int argc, char *argv[] ) {
 
 	/* worker process code */
 
-	printf("Worker %d - Spawned\n", myrank);
+	printf("Worker (%d, %d): Spawned\n", getpid(), myrank);
 
-	// Receive array dimension and threshold k
-	MPI_Bcast( nm_array, NR_WORKERS, MPI_INT, 0, parentcomm );
-	n = nm_array[0];
-	k = nm_array[1];
+	// Receive from parent
+	MPI_Bcast( send_array, 4, MPI_INT, 0, parentcomm );
+	printf("Worker (%d, %d): Spawned and received!\n", getpid(), myrank);
+	m = send_array[0];
+	n = send_array[1];
+	k = send_array[2];
+	root = send_array[3];
 
-	array = malloc(n*sizeof(int));
+	array = malloc(n * sizeof(int));
 	sum_array = malloc(sizeof(int));
 
-	// Receive array
 	MPI_Scatter(array, n, MPI_INT, array, n, MPI_INT, 0, parentcomm);
-	printf("Final Worker %d: Receiving: ", myrank);
+	printf("Worker (%d, %d) with ROOT %d: Receiving: m = %d n = %d, k = %d and nr: ", getpid(), myrank, root, m, n, k);
 	print_array(array, n, myrank);
 
-	if(n <= k) {
+	if(n <= k) // leaf worker
+	{
+		printf("Leaf Worker (%d, %d)\n", getpid(), myrank);
 		// Compute sum of array
 		sum = compute_sum(array, n);
-		printf("Final Worker %d: Sum = %d\n", myrank, sum);
+		printf("Leaf Worker (%d, %d): Sum = %d\n", getpid(), myrank, sum);
 
 		// Send sum to parent
 		MPI_Gather(&sum, 1, MPI_INT, sum_array, 1, MPI_INT, 0, parentcomm);
 	}
-	else {
-		printf("Worker %d: Receiving: ", myrank);
-		print_array(array, n, myrank);
-		printf("Worker %d: Spawning other workers\n", myrank);
+	else // internal worker
+	{
+		printf("Worker (%d, %d): Spawning other workers\n", getpid(), myrank);
 
 		// Spawn workers
-		MPI_Comm_spawn( "distributed_sum_child", MPI_ARGV_NULL, NR_WORKERS, MPI_INFO_NULL, 0, MPI_COMM_WORLD, &workercomm2, MPI_ERRCODES_IGNORE );
+		MPI_Comm_spawn("distributed_sum_child", MPI_ARGV_NULL, NR_WORKERS, MPI_INFO_NULL, 0, MPI_COMM_SELF, &workercomm, MPI_ERRCODES_IGNORE);
 
-		nm_array[0] = n/2;
+		printf("Worker (%d, %d): Finished Spawning\n", getpid(), myrank);
+
+		send_array[1] = n/2;
+		send_array[3] = getpid();
 
 		// Send matrix dimensions to workers
-		MPI_Bcast(nm_array, NR_WORKERS, MPI_INT, myrank == 0 ? MPI_ROOT : MPI_PROC_NULL, workercomm2);
+		MPI_Bcast(send_array, 4, MPI_INT, MPI_ROOT, workercomm);
 
-		sub_array = malloc(n*sizeof(int));
+		sub_array = malloc((n/2) * sizeof(int));
 
-		// Send matrix line to worker
-		MPI_Scatter(array, n/2, MPI_INT, sub_array, n/2, MPI_INT, myrank == 0 ? MPI_ROOT : MPI_PROC_NULL, workercomm2);
+		// Send matrix line to workers
+		MPI_Scatter(array, n/2, MPI_INT, sub_array, n/2, MPI_INT, MPI_ROOT, workercomm);
 
 		sum_array = malloc(NR_WORKERS * sizeof(int));
 
 		// Receive partial sums from workers
-		MPI_Gather(&sum, 1, MPI_INT, sum_array, 1, MPI_INT, myrank == 0 ? MPI_ROOT : MPI_PROC_NULL, workercomm2);
+		MPI_Gather(&sum, 1, MPI_INT, sum_array, 1, MPI_INT, MPI_ROOT, workercomm);
 
 		// Compute final sum
 		sum = compute_sum(sum_array, NR_WORKERS);
-		printf("Worker %d: Cumulative Sum = %d\n", myrank, sum);
+		printf("Worker (%d, %d): Cumulative Sum = %d\n", getpid(), myrank, sum);
 
 		// Send sum to parent
 		MPI_Gather(&sum, 1, MPI_INT, sum_array, 1, MPI_INT, 0, parentcomm);
 
-		MPI_Comm_free( &workercomm2 );
+		MPI_Comm_free( &workercomm );
 	}
 
-	/* worker process code */
+	/* end worker process code */
+
 	MPI_Comm_free( &parentcomm );
 
 	MPI_Finalize( );
